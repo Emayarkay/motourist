a64b13ab24bc485b92403a8db606f8437bed1f3d
Elliot Griffin <elliotg283@gmail.com>
2023-11-20 16:27:01 +0000

    created 16.26

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :booking_models
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

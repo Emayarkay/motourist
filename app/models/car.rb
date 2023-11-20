acb4f572a0ff44233e642124b6e1f0b6cd8de49e
Jonny Shipton <jonathanshipton1@gmail.com>
2023-11-20 17:08:19 +0000

    Added validations to car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,9 @@
 class Car < ApplicationRecord
+  validates :make, :model, :year, :price, :description, :location, :colour, :capacity, presence: true
+  validates :year, presence: true
+  validates :price, presence: true
+  validates :description, length: { minimum: 10 }
+  validates :year, numericality: { only_integer: true }
+  validates :year, comparison: { greater_than: 1886 }
+  validates :year, comparison: { less_than: 2023 }
 end

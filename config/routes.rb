b49e73b8f4aae8dbe0e5349ed952b815328342aa
Elliot Griffin <elliotg283@gmail.com>
2023-11-21 11:54:27 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -19,4 +20,16 @@ Rails.application.routes.draw do
   resources :cars
 
   # Defines the root path route ("/")
+
+
+  resources :users, only: [:new, :create, :edit, :update, :destroy] do
+    member do
+      get 'confirm_changes'
+    end
+  end
+
+  resources :users, only: [:show] do
+    get 'car_listings', on: :member
+  end
+
 end

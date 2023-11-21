ba6a2f79a5cfdd6a87e70906f4964ec8247407e9
Elliot Griffin <elliotg283@gmail.com>
2023-11-21 13:46:01 +0000

    made changes to controller

--- config/routes.rb
+++ config/routes.rb
@@ -21,14 +21,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
 
-
-  resources :users, only: [:new, :create, :edit, :update, :destroy] do
-    member do
-      get 'confirm_changes'
-    end
-  end
-
-  resources :users, only: [:show] do
+resources :users, only: [:show] do
     get 'car_listings', on: :member
   end
 

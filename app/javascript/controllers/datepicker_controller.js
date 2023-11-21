b64dd2d5063072287e4d8983d1bbfde29050a816
Robbie Bullough <robbie.bullough@hotmail.co.uk>
2023-11-21 14:05:19 +0000

    Added elements to index/show pages

---
+++ app/javascript/controllers/datepicker_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="datepicker"
+export default class extends Controller {
+  connect() {
+    flatpickr(this.element, {
+      minDate: "today",
+      mode: "range"
+    })
+  }
+}

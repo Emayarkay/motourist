33f8ace8ea1dea98eb82e5a4bf8ff7c449d69d1d
Elliot Griffin <elliotg283@gmail.com>
2023-11-23 13:41:50 +0000

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_131203) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_23_112242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,18 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_131203) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "booking_models", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_booking_models_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_booking_models_on_reset_password_token", unique: true
   end
 
   create_table "bookings", force: :cascade do |t|
@@ -76,6 +88,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_131203) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
